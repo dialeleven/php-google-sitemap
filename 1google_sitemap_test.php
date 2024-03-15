e615237c8700ad8c1f7c9ef1665020d4042dd540
Francis Tsao <dialeleven@gmail.com>
2024-03-14 22:55:26 -0400

    construct() down to ONE param. Using a bunch of setter methods which is easier to read rather than 6-7 parameters (ugh).

--- 1google_sitemap_test.php
+++ 1google_sitemap_test.php
@@ -49,15 +49,15 @@ while ($query_data = $stmt->fetch())
 }
 
 
-$my_sitemap = new GoogleSitemap($pdo, $sql_total, $http_host = $_SERVER['HTTP_HOST']);
+$my_sitemap = new GoogleSitemap($http_host = $_SERVER['HTTP_HOST']);
 
 // is this script not in the root/public dir? enter the number of directories deep we are in (e.g. /in/here/google_sitemap.php = "2")
 #$my_sitemap->setPathAdjustmentToRootDir($path_adj = 0);
 
-
+$my_sitemap->setUseMysqlDbModeFlag(true, $pdo, $sql_total); // generate URLs for sitemap from MySQL? true/false, your PDO object, basic SQL "COUNT(*) AS total"
 $my_sitemap->setSitemapFilenamePrefix('mysitemap'); // set name of sitemap file minus ".xml" (e.g. mysitemap.xml)
 $my_sitemap->setSitemapChangeFreq('weekly'); // set sitemap 'changefreq' how often the content is expected to change (always, hourly, daily, weekly, monthly, yearly, never)
-$my_sitemap->setHostnamePrefixFlag(1); // set to 1 to use "https://$_SERVER['HTTP_HOST]/"+REST-OF-YOUR-URL-HERE/. 0 if using full URLs.
+$my_sitemap->setHostnamePrefixFlag(true); // 'true' to use "https://$_SERVER['HTTP_HOST]/"+REST-OF-YOUR-URL-HERE/. 'false' if using full URLs.
 ?>
 
 
