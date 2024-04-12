bbc911d0f6a61da24fd23043c9dd65cc4f68c616
Francis Tsao <dialeleven@gmail.com>
2024-04-12 10:55:35 -0400

    moved startXmlDoc() as concrete method, moved required constant props to abstract class from reviewing PHPUnit errors

--- src/AbstractGoogleSitemap.php
+++ src/AbstractGoogleSitemap.php
@@ -12,6 +12,12 @@ namespace Dialeleven\PhpGoogleXmlSitemap;
 
 abstract class GoogleSitemap
 {
+   const MAX_SITEMAP_LINKS = 50000;
+   #const MAX_SITEMAP_LINKS = 5;
+   const SITEMAP_FILENAME_SUFFIX = '.xml';
+   //const MAX_FILESIZE = 10485760;       // 10MB maximum (unsupported feature currently)
+
+   
    abstract protected function startXmlNsElement(string $xml_ns_type = 'sitemapindex'): bool;
    abstract protected function startNewUrlsetXmlFile(): void;
    abstract public function addUrl(string $url, string $lastmod = '', string $changefreq = '', string $priority = ''): bool;
@@ -19,4 +25,54 @@ abstract class GoogleSitemap
    abstract protected function gzipXmlFiles(): bool;
    abstract protected function generateSitemapIndexFile(): bool;
    abstract protected function outputXml(): bool;
+
+   /**
+     * Start the XML document. Use either 'memory' mode to send to browser or 'openURI()'
+     * save as a file with the specified filename. Set our indentation and then of course
+     * start with the <?xml version="1.0" encoding="UTF-8"?> tag.
+     * @access protected
+     * @param  string $xml_ns_type  values ('urlset' or 'sitemapindex') create either a <urlset xmlns> tag or <sitemapindex> tag
+     * @return bool
+     */      
+   public function startXmlDoc(string $xml_ns_type = 'urlset'): bool
+   {
+      // Set the output to memory (for testing mainly)
+      if ($this->xml_mode == 'memory')
+      {
+         $this->xml_writer->openMemory();
+      }
+      // file writing mode
+      else if ($this->xml_mode == 'file')
+      {
+         // sitemapindex will be "userspecifiedname_index.xml"
+         if ($xml_ns_type == 'sitemapindex')
+         {
+            $uri = $this->xml_files_dir . "{$this->sitemap_filename_prefix}_index" . self::SITEMAP_FILENAME_SUFFIX;
+            $uri_return_val = $this->xml_writer->openURI($uri);
+         }
+         // urlset file
+         else
+         {
+            $uri = $this->xml_files_dir . $this->sitemap_filename_prefix . ($this->num_sitemaps + 1) . self::SITEMAP_FILENAME_SUFFIX;
+            $uri_return_val = $this->xml_writer->openURI($uri);
+         }
+
+         // error opening the URI - path error or directory doesn't exist
+         if ($uri_return_val == false) { throw new Exception("Error opening '$uri.' Please check your directory path and that the directory exists.***"); }
+      }
+
+
+      // Set indentation and line breaks for readability
+      $this->xml_writer->setIndent(true);
+      $this->xml_writer->setIndentString('   '); // Adjust the number of spaces for indentation as desired
+
+
+      // Start the document with XML declaration and encoding
+      $this->xml_writer->startDocument('1.0', 'UTF-8');
+
+      // open our cotainting tag either 'sitemapindex' or 'urlset'
+      $this->startXmlNsElement($xml_ns_type = 'urlset');
+
+      return true;
+   }
 }
--- src/GoogleXmlSitemap.php
+++ src/GoogleXmlSitemap.php
@@ -45,10 +45,6 @@ require_once 'AbstractGoogleSitemap.php';
 
 class GoogleXmlSitemap extends GoogleSitemap
 {
-   const MAX_SITEMAP_LINKS = 50000;
-   #const MAX_SITEMAP_LINKS = 5;
-   const SITEMAP_FILENAME_SUFFIX = '.xml';
-   //const MAX_FILESIZE = 10485760;       // 10MB maximum (unsupported feature currently)
    
    protected $xml_writer;
    protected $current_url_count = 0; // total number of <loc> URL links for current <urlset> XML file
@@ -197,55 +193,7 @@ class GoogleXmlSitemap extends GoogleSitemap
 
    /////////////////////// NEW XMLwriter methods ///////////////////////////
 
-   /**
-     * Start the XML document. Use either 'memory' mode to send to browser or 'openURI()'
-     * save as a file with the specified filename. Set our indentation and then of course
-     * start with the <?xml version="1.0" encoding="UTF-8"?> tag.
-     * @access protected
-     * @param  string $xml_ns_type  values ('urlset' or 'sitemapindex') create either a <urlset xmlns> tag or <sitemapindex> tag
-     * @return bool
-     */      
-   protected function startXmlDoc(string $xml_ns_type = 'urlset'): bool
-   {
-      // Set the output to memory (for testing mainly)
-      if ($this->xml_mode == 'memory')
-      {
-         $this->xml_writer->openMemory();
-      }
-      // file writing mode
-      else if ($this->xml_mode == 'file')
-      {
-         // sitemapindex will be "userspecifiedname_index.xml"
-         if ($xml_ns_type == 'sitemapindex')
-         {
-            $uri = $this->xml_files_dir . "{$this->sitemap_filename_prefix}_index" . self::SITEMAP_FILENAME_SUFFIX;
-            $uri_return_val = $this->xml_writer->openURI($uri);
-         }
-         // urlset file
-         else
-         {
-            $uri = $this->xml_files_dir . $this->sitemap_filename_prefix . ($this->num_sitemaps + 1) . self::SITEMAP_FILENAME_SUFFIX;
-            $uri_return_val = $this->xml_writer->openURI($uri);
-         }
-
-         // error opening the URI - path error or directory doesn't exist
-         if ($uri_return_val == false) { throw new Exception("Error opening '$uri.' Please check your directory path and that the directory exists.***"); }
-      }
-
-
-      // Set indentation and line breaks for readability
-      $this->xml_writer->setIndent(true);
-      $this->xml_writer->setIndentString('   '); // Adjust the number of spaces for indentation as desired
-
-
-      // Start the document with XML declaration and encoding
-      $this->xml_writer->startDocument('1.0', 'UTF-8');
-
-      // open our cotainting tag either 'sitemapindex' or 'urlset'
-      $this->startXmlNsElement($xml_ns_type = 'urlset');
-
-      return true;
-   }
+   
 
 
    /**
